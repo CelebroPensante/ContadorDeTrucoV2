405ce86a396f273e7684d4371c541c68ed1f72fc
pedrolbp <pedrolbarriola@gmail.com>
2025-11-13 23:08:48 -0300

    atualizado algumas coisas

--- app/src/main/java/com/example/contadordetruco/MainActivity.kt
+++ app/src/main/java/com/example/contadordetruco/MainActivity.kt
@@ -4,9 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.database
 
 class MainActivity : AppCompatActivity() {
 
